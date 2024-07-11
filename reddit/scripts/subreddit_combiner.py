437197175219e0203122c5e80cc633b38c3d761c
saad mankarious <saadmankarious@gmail.com>
2024-07-11 20:16:43 +0200

    change subreddit combiner logic

--- reddit/scripts/subreddit_combiner.py
+++ reddit/scripts/subreddit_combiner.py
@@ -2,43 +2,28 @@ import os
 import json
 import argparse
 
-def process_file(filepath, authors_data):
-    with open(filepath, 'r') as file:
-        for line in file:
-            data = json.loads(line)
-            author = data.get('author')
-            if author:
-                if author not in authors_data:
-                    authors_data[author] = {'comments': [], 'posts': []}
-                if 'body' in data:
-                    authors_data[author]['comments'].append(data)
-                elif 'title' in data:
-                    authors_data[author]['posts'].append(data)
+def process_files(input_folder):
+    comments_output_path = os.path.join(input_folder, 'combined_commentsw.jsonl')
+    posts_output_path = os.path.join(input_folder, 'combined_postsw.jsonl')
 
-def save_combined_data(authors_data, comments_output_file, posts_output_file):
-    with open(comments_output_file, 'w') as comments_file, open(posts_output_file, 'w') as posts_file:
-        for author, content in authors_data.items():
-            for comment in content['comments']:
-                comments_file.write(json.dumps(comment) + '\n')
-            for post in content['posts']:
-                posts_file.write(json.dumps(post) + '\n')
-
-def main(input_directory, comments_output_file, posts_output_file):
-    authors_data = {}
-    for root, _, files in os.walk(input_directory):
-        for file in files:
-            print(f'read file {file}')
-            if file.endswith("_comments.jsonl") or file.endswith("_posts.jsonl"):
-                process_file(os.path.join(root, file), authors_data)
-    save_combined_data(authors_data, comments_output_file, posts_output_file)
+    with open(comments_output_path, 'w') as comments_outfile, open(posts_output_path, 'w') as posts_outfile:
+        for filename in os.listdir(input_folder):
+            if filename.endswith('_comments.jsonl'):
+                with open(os.path.join(input_folder, filename), 'r') as infile:
+                    for line in infile:
+                        comments_outfile.write(line)
+            elif filename.endswith('_posts.jsonl'):
+                with open(os.path.join(input_folder, filename), 'r') as infile:
+                    for line in infile:
+                        posts_outfile.write(line)
+            print(f'processed file {filename}')
+    
+    print(f"Combined comments written to: {comments_output_path}")
+    print(f"Combined posts written to: {posts_output_path}")
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Combine JSONL files into single comments and posts files grouped by author.')
-    parser.add_argument('input_directory', type=str, help='Path to the input directory containing the JSONL files')
+    parser = argparse.ArgumentParser(description='Combine JSONL files for comments and posts.')
+    parser.add_argument('input_folder', type=str, help='The input folder containing the JSONL files.')
     args = parser.parse_args()
 
-    input_directory = args.input_directory
-    comments_output_file = args.input_directory + '/combined_comments.jsonl'
-    posts_output_file =  args.input_directory + '/combined_posts.jsonl'
-    
-    main(input_directory, comments_output_file, posts_output_file)
+    process_files(args.input_folder)
